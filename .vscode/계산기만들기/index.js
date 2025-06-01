3950b77780ebcfbe10924f947fee58c5f7e838e2
선아 <msa200827@gmail.com>
2025-06-01 11:33:52 +0900

    계산기만들기 js부분

--- .vscode/계산기만들기/index.js
+++ .vscode/계산기만들기/index.js
@@ -14,27 +14,23 @@ function onSubmit(event) {
   //   드롭다운에서 선택된 값을 가져옴
   let resultValue;
   //   나중에 결과값을 넣기위한 빈 변수
-  if (oper === '+') {
-    resultValue = num1 + num2
-  }
-  else if  (oper === '-'){
-    resultValue = num1 - num2
-  }
-  else if (oper === '*') {
-    resultValue = num1 * num2
-  }
-  else if (oper === '/') {
-    if (num2 === 0){
-    result.innerText = "0으로 나눌 수 없습니다";
-        return; 
-        // 함수실행을 멈춰야 Infinity가 뜨지 않는다
+  if (oper === "+") {
+    resultValue = num1 + num2;
+  } else if (oper === "-") {
+    resultValue = num1 - num2;
+  } else if (oper === "*") {
+    resultValue = num1 * num2;
+  } else if (oper === "/") {
+    if (num2 === 0) {
+      result.innerText = "0으로 나눌 수 없습니다";
+      return;
+      // 함수실행을 멈춰야 Infinity가 뜨지 않는다
     }
-    resultValue = num1 / num2
+    resultValue = num1 / num2;
   }
-  
 
-  result.innerText = `결과는 ${resultValue}입니다`
+  result.innerText = `결과는 ${resultValue}입니다`;
 }
-calculation.addEventListener('submit', onSubmit);
+calculation.addEventListener("submit", onSubmit);
 // 폼이 제출(submit)될 때, onSubmit이라는 함수를 실행
 // submit은 이벤트 이름이기 때문에 문자열로 감싼다
