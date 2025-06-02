899904efe50b94ca1950cd1483064101c3e66ffe
chaeyn <chaulfe@gmail.com>
2025-06-03 01:01:18 +0900

    playlist

--- playlist2/main.js
+++ playlist2/main.js
@@ -1,3 +1,14 @@
-.container {
+const audio = document.getElementById("myAudio");
+const progressBar = document.getElementById("progressBar");
 
-}
+audio.addEventListener("loadedmetadata", function () {
+  progressBar.max = audio.duration;
+});
+
+audio.addEventListener("timeupdate", function () {
+  progressBar.value = audio.currentTime;
+});
+
+progressBar.addEventListener("input", function () {
+  audio.currentTime = progressBar.value;
+});
