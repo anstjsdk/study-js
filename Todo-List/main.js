9f1d6a520341216a5cda50256372632cffbf3b0e
선아 <msa200827@gmail.com>
2025-06-02 12:00:09 +0900

    js 문제풀기

--- Todo-List/main.js
+++ Todo-List/main.js
@@ -82,7 +82,12 @@ if (savedToDos) {
 }
 
 // element의 id를 받아 와서 상수에 저장
-
+const a = document.getElementById("element");
 // name, id를 가진 userInfo 라는 Object를 생성
-
+const userInfo = {
+  name: toDos,
+  id: todolist,
+};
 // button에 click이라는 이벤트가 발생하면 deleteUser이라는 함수를 실행
+const button = document.getElementById("button");
+button.addEventListener("click",deleteuser)
