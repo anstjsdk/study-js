3edf2eb2a88171e691617da2e5b4c63c5a92b5b6
선아 <msa200827@gmail.com>
2025-06-04 23:51:41 +0900

    js문제풀기

--- practice1/main.js
+++ practice1/main.js
@@ -5,12 +5,22 @@
 // let hihi = sayhello("빵");
 // console.log(hihi);
 //두 수 입력받아서 계산
-function add(a,b) {
-    return a + b;
+// function add(a,b) {
+//     return a + b;
+// }
+// let num1 = parseInt(prompt('첫번째 수를 입력하세요'));
+// let num2 = parseInt(prompt('두번째 수를 입력하세요'));
+// let a = num1;
+// let b = num2;
+// const result = add(num1,num2)
+// console.log(result)
+
+function number(a) {
+  if (a % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
-let num1 = parseInt(prompt('첫번째 수를 입력하세요'));
-let num2 = parseInt(prompt('두번째 수를 입력하세요'));
-let a = num1;
-let b = num2;
-const result = add(num1,num2)
-console.log(result)
+let num = parseInt(prompt("수를 입력하세요"));
+console.log(number(number));
